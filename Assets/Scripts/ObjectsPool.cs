ef5b231bed65914c0a66255b0fa6a949c6ea8efb
MoonPalli <crashik911@gmail.com>
2021-03-05 04:39:45 +0300

    Initial commit

---
+++ Assets/Scripts/ObjectsPool.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ObjectsPool : MonoBehaviour
+{
+    [SerializeField] private GameObject _container;
+
+    private List<GameObject> _objPool = new List<GameObject>();
+
+    protected void Initialize(GameObject prefab, int count)
+    {
+        for (int i = 0; i <= count - 1; i++)
+        {
+            GameObject objToSpawn = Instantiate(prefab, _container.transform);
+            objToSpawn.SetActive(false);
+
+            _objPool.Add(objToSpawn);
+        }
+    }
+
+    protected bool TryGetObject(out GameObject obj)
+    {
+        obj = _objPool.FirstOrDefault(o => o.activeSelf == false);
+
+        return obj != null;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Spawner : ObjectsPool
+{
+    [SerializeField] private Transform _spawnPoints;
+    [SerializeField] private GameObject _objectPrefab;
+    [SerializeField] private float _secondsBetweenSpawn;
+
+    private Transform[] _points;
+    private float _elapsedTime;
+    private int _currentPoint = 0;
+
+    private void Start()
+    {
+        _points = new Transform[_spawnPoints.childCount];
+        Initialize(_objectPrefab, _spawnPoints.childCount);
+
+        for (int i = 0; i < _points.Length; i++)
+            _points[i] = _spawnPoints.GetChild(i);
+    }
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime >= _secondsBetweenSpawn)
+        {
+            if (TryGetObject(out GameObject enemy))
+            {
+                _elapsedTime = 0;
+
+                SetObject(enemy, _points[_currentPoint].position);
+
+                _currentPoint += 1;
+            }
+        }
+    }
+
+    private void SetObject(GameObject obj, Vector2 spawnPoint)
+    {
+        obj.SetActive(true);
+        obj.transform.position = spawnPoint;
+    }
+}
